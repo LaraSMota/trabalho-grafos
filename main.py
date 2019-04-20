b322afe9501ea2a8ba123797e46f9fed8fc31388
MotaLara <lara.s.mota@gmail.com>
2019-04-20 16:33:43 -0300

    (menu): alterei o menu para lidar apenas com um grafo e alterei a ordem das opções do menu

--- main.py
+++ main.py
@@ -21,21 +21,10 @@ def imprime_arestas(msg, arestas):
   print(msg, a_concatenado, '\n')
 
 def seleciona_funcionalidade(opcao):
-  if opcao == 1:
-    nome_grafo = input('Digite nome do grafo: ')
-    direcionado = input('É direcionado?(s ou n) ')
-    while direcionado != 's' and direcionado != 'n':
-      direcionado = input('Digite \'s\' ou \'n\': ')
-    if direcionado == 's':
-      ehDirecionado = True
-    else:
-      ehDirecionado = False
-    global grafo
-    grafo = Grafo(nome_grafo, ehDirecionado)
-  elif opcao == 2:
+  if opcao == 1:  #INSERIR VERTICES
     vertices = input('Digite os vértices: ')
     grafo.criar_vertices(vertices)
-  elif opcao == 3:
+  elif opcao == 2:  #INSERIR ARESTA
     imprime_vertices('Vértices existentes: ', grafo.vertices)
     aresta_criada = False
     while aresta_criada == False:
@@ -46,21 +35,22 @@ def seleciona_funcionalidade(opcao):
       vertice_2 = input('Digite o vertice 2: ').upper()
       peso = int(input('Digite o peso: '))
       aresta_criada = grafo.criar_aresta(id, vertice_1, vertice_2, peso)
+  elif opcao == 3:  #DELETAR VERTICE
+    imprime_vertices('Vértices criados: ', grafo.vertices)
+    id_vertice = input('Digite o id do vértice: ').upper()
+    grafo.deleta_vertice(id_vertice)
+  #DELETAR ARESTA
   elif opcao == 4:
     imprime_arestas('Arestas existentes: ', grafo.arestas)
     id_aresta = input('Digite o id da aresta: ').upper()
     aresta_deletada = grafo.deleta_aresta(id_aresta)
     if not(aresta_deletada):
       print('A aresta deve ter sido criada para realizar essa ação')
-  elif opcao == 5:
-    imprime_vertices('Vértices criados: ', grafo.vertices)
-    id_vertice = input('Digite o id do vértice: ')
-    grafo.deleta_vertice(id_vertice)
-  elif opcao == 7:
+  elif opcao == 5:  #TESTAR EXISTENCIA DE ARESTA
     id_vertice = input('Digite o id do vértice: ').upper()
     vertices_adjacentes = grafo.get_vertices_adjacentes(id_vertice)
     imprime_vertices('Vértices adjacentes ao vértice {}:'.format(id_vertice), vertices_adjacentes)
-  elif opcao == 14:
+  elif opcao == 13:  #IMPRIME VERTICES E ARESTAS
     print("Grafo: " + grafo.nome + "\n")
     imprime_vertices('Vértices existentes: ', grafo.vertices)
     imprime_arestas('Arestas existentes: ', grafo.arestas)
@@ -69,28 +59,40 @@ def seleciona_funcionalidade(opcao):
 
 def display_menu():
   print('''
-  1 - Criar grafo
-  2 - Inserir vértices
-  3 - Inserir aresta
+  1 - Inserir vértices
+  2 - Inserir aresta
+  3 - Remover vértices
   4 - Remover aresta
-  5 - Remover vértices
-  6 - Testar existência de aresta entre dois vértices
-  7 - Obter os vértices adjacentes a um determinado vértice
-  8 - Obter o grau de um determinado vértice
-  9 - Obter o grau médio, o grau mínimo e o grau máximo
-  10 - Identifique se o grafo é conexo
-  11 - Exibir Matriz de Adjacências
-  12 - Verificar existência de um caminho de Euler
-  13 - Ler entrada a partir de arquivo
-  14 - Visualizar vertices e arestas
-  15 - Sair\n''')
+  5 - Testar existência de aresta entre dois vértices
+  6 - Obter os vértices adjacentes a um determinado vértice
+  7 - Obter o grau de um determinado vértice
+  8 - Obter o grau médio, o grau mínimo e o grau máximo
+  9 - Identifique se o grafo é conexo
+  10 - Exibir Matriz de Adjacências
+  11 - Verificar existência de um caminho de Euler
+  12 - Ler entrada a partir de arquivo
+  13 - Visualizar vertices e arestas
+  14 - Sair\n''')
 
-while opcao != 15:
+print("Seja bem vindo! \n")
+nome_grafo = input('Digite nome do grafo: ')
+direcionado = input('É direcionado?(s ou n) ')
+while direcionado != 's' and direcionado != 'n':
+  direcionado = input('Digite \'s\' ou \'n\': ')
+if direcionado == 's':
+  ehDirecionado = True
+else:
+  ehDirecionado = False
+global grafo
+grafo = Grafo(nome_grafo, ehDirecionado)
+
+while opcao != 14:
   display_menu()
   opcao = int(input('Selecione uma opção: '))
   print('----------------------- \n')
   seleciona_funcionalidade(opcao)
 
+print("FIM DE EXECUÇÃO")
 # SETUP DE TESTES
 # grafo = Grafo('Grafo 1')
 # vertices = input('Digite os vértices')
